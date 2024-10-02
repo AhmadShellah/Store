5d220a244d53d852b0493356eca60198de29ad23
Ahmad Shellah <ahmad.yaseen.shellah@gmail.com>
2024-10-02 21:54:30 +0300

    Add Models

---
+++ Objects/Dtos/InvoiceDto/InvoiceDto.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Objects.Dtos.InvoiceDto
+{
+    public class InvoiceDto : BaseEntityDto
+    {
+        public string PersonName { get; set; }
+
+        public int Number { get; set; }
+
+        public DateTime DueDate { get; set; }
+
+        public decimal Total { get; set; }
+    }
+}
---
+++ Objects/Models/InvoiceModels/InvoiceModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Objects.Models.InvoiceModels
+{
+    public class InvoiceModel
+    {
+        public Guid Id { get; set; }
+
+        public string PersonName { get; set; }
+
+        public int Number { get; set; }
+
+        public DateTime DueDate { get; set; }
+
+        public decimal Total { get; set; }
+
+        public List<ProductModel> Products { get; set; }
+    }
+}
